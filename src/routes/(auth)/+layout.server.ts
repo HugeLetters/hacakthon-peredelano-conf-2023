28de841d6f695fcb9ad7ea495299db3c14c09b26
hugeletters <ckuka71@gmail.com>
2023-10-31 19:42:52 +0100

    return session in loader so it's type is narrowed

--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -4,4 +4,6 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ parent, url }) {
 	const { session } = await parent();
 	if (!session) throw redirect(302, `/signin?${CALLBACK_URL_KEY}=${url.toString()}`);
+
+	return { session };
 }
--- src/routes/(noauth)/signin/+page.server.ts
+++ src/routes/(noauth)/signin/+page.server.ts
@@ -3,4 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ parent }) {
 	const { session } = await parent();
 	if (session) throw redirect(302, '/');
+
+	return { session };
 }
