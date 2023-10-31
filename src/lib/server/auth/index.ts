65f598463f52e90435455ad9a5e3e4d2ad7ca57f
hugeletters <ckuka71@gmail.com>
2023-10-31 19:46:33 +0100

    moved host url to .env

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -1,12 +1,13 @@
 import { dev } from '$app/environment';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { PUBLIC_HOST_URL } from '$env/static/public';
 import { libsql } from '@lucia-auth/adapter-sqlite';
+import { google } from '@lucia-auth/oauth/providers';
 import { getTableConfig } from 'drizzle-orm/sqlite-core';
 import { lucia } from 'lucia';
 import { sveltekit } from 'lucia/middleware';
 import { libsqlClient } from '../database';
 import { user, userKey, userSession } from '../database/schema/auth';
-import { google } from '@lucia-auth/oauth/providers';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = lucia({
 	adapter: libsql(libsqlClient, {
@@ -24,8 +25,7 @@ export const auth = lucia({
 export const googleAuth = google(auth, {
 	clientId: GOOGLE_CLIENT_ID,
 	clientSecret: GOOGLE_CLIENT_SECRET,
-	// todo - host should by set by env
-	redirectUri: 'http://localhost:5173/api/auth/signin/google/callback'
+	redirectUri: `${PUBLIC_HOST_URL}/api/auth/signin/google/callback`
 });
 
 export type Auth = typeof auth;
