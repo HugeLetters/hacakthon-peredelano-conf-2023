d20ce40c036025b4f597e77b5c1b585add979657
hugeletters <ckuka71@gmail.com>
2023-11-01 15:32:56 +0100

    use href instead of tostring

--- src/routes/(auth)/+layout.server.ts
+++ src/routes/(auth)/+layout.server.ts
@@ -8,7 +8,7 @@ export async function load({ locals, url, depends }) {
 	// this loader function should trigger on each URL change
 	// but because URL is referenced in the condition only, its doesnt trigger
 	// which actually is kinda what we need anyways :)
-	if (!session) throw redirect(302, getSignInUrl(url.toString()));
+	if (!session) throw redirect(302, getSignInUrl(url.href));
 
 	return { session };
 }
