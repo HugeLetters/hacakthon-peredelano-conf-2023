9f03d490a7351fce95cced84005c0d2c05beae38
hugeletters <ckuka71@gmail.com>
2023-11-08 01:20:58 +0100

    melt-ui

--- src/routes/(auth)/+page.svelte
+++ src/routes/(auth)/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { createPopover, melt } from '@melt-ui/svelte';
 	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
 
 	export let data;
 
@@ -11,6 +13,11 @@
 			data.trpc.greet.utils.invalidate();
 		}, 500);
 	});
+
+	const {
+		elements: { trigger, content, arrow },
+		states: { open }
+	} = createPopover({ forceVisible: true });
 </script>
 
 <h1 class="header">Welcome to SvelteKit {$query.data} - {$query2.data}</h1>
@@ -18,8 +25,26 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/other">other page</a>
 
+<button use:melt={$trigger} class="popover"> open popover </button>
+{#if $open}
+	<div use:melt={$content} transition:fly={{ y: -10 }} class="popover-content">
+		<div use:melt={$arrow} />
+		popover content
+	</div>
+{/if}
+
 <style lang="scss">
 	.header {
 		background: linear-gradient(to left, yellow, green);
+	}
+	.popover {
+		background-color: green;
+		border-radius: 1rem;
+		padding: 1rem;
+	}
+	.popover-content {
+		background-color: aqua;
+		border-radius: 0.5rem;
+		padding: 0.5rem;
 	}
 </style>
