742bb17be000d96e48ac9cf2d1c50de5235be4e6
hugeletters <ckuka71@gmail.com>
2023-11-10 12:58:44 +0100

    improved inferSQLSelectModel type a bit

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,13 +1,17 @@
 import { createClient } from '@libsql/client';
+import type { InferSelectModel } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/libsql';
 import { text, type SQLiteColumn, type SQLiteTable } from 'drizzle-orm/sqlite-core';
 
 export const libsqlClient = createClient({ url: 'file:./database/db.sqlite' });
 export const db = drizzle(libsqlClient);
 
-export type InferSQLSelectModel<T extends SQLiteTable> = {
-	[K in keyof T as T[K] extends SQLiteColumn ? T[K]['_']['name'] : never]: T[K] extends SQLiteColumn
-		? T[K]['_']['data'] | (T[K]['_']['notNull'] extends true ? never : null)
+export type InferSQLSelectModel<
+	T extends SQLiteTable,
+	S extends InferSelectModel<T> = InferSelectModel<T>
+> = {
+	[K in keyof T as T[K] extends SQLiteColumn ? T[K]['_']['name'] : never]: K extends keyof S
+		? S[K]
 		: never;
 };
 
