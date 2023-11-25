01e6f6e5aba2f70e6c717fbe4e25b13100dacd52
hugeletters <ckuka71@gmail.com>
2023-11-25 19:52:56 +0100

    update case data

--- src/routes/(auth)/(admin)/case/[id]/+page.svelte
+++ src/routes/(auth)/(admin)/case/[id]/+page.svelte
@@ -2,10 +2,25 @@
 	export let data;
 
 	const caseInfo = data.trpc.case.caseInfo.query({ caseId: data.caseId });
+
+	const caseInfoMutation = data.trpc.case.updateCaseData.mutation();
 </script>
 
 <div>
 	case {data.caseId}
 
 	<div>{$caseInfo.data?.summary}</div>
+	<button
+		on:click={() => {
+			$caseInfoMutation.mutate({
+				caseId: data.caseId,
+				newSummary: 'new summary',
+				newStatus: 'closed'
+			});
+
+			data.trpc.case.caseInfo.utils.invalidate({ caseId: data.caseId });
+		}}
+	>
+		change summary
+	</button>
 </div>
