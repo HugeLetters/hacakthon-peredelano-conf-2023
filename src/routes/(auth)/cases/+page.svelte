d0310413a2cdc4d98a1d5754b95f8ab53d766dae
Babulekh <vanca624@gmail.com>
2023-11-25 19:13:11 +0100

    Added popover filters

--- src/routes/(auth)/cases/+page.svelte
+++ src/routes/(auth)/cases/+page.svelte
@@ -1,15 +1,49 @@
 <script lang="ts">
 	import Bank from '$lib/icons/bank.svelte';
+	import Filter from '$lib/icons/filter.svelte';
+	import Arrow from '$lib/icons/arrow.svelte';
 
 	export let data;
+	let isPopoverOpened: boolean = false;
+	let filterRows: {
+		title: string;
+		filterId: string;
+		buttons: { name: string; value: string }[];
+	}[] = [
+		{
+			title: 'Статус',
+			filterId: 'status',
+			buttons: [
+				{ name: 'Todo', value: 'todo' },
+				{ name: 'In process', value: 'inprocess' },
+				{ name: 'Done', value: 'done' }
+			]
+		}
+	];
+	export let selectedFilters: Object = {};
 
 	const cases = data.trpc.case.findMany.query();
+
+	function toggleFilter(filterId, value) {
+		if (selectedFilters[filterId] === value) selectedFilters[filterId] = null;
+		else selectedFilters[filterId] = value;
+	}
 </script>
 
 <div class="cases">
-	{#if $cases.isSuccess}
+	<div class="header">
+		<button
+			class="headerButton"
+			type="button"
+			on:click={() => (isPopoverOpened = !isPopoverOpened)}
+		>
+			<svelte:component this={isPopoverOpened ? Arrow : Filter} />
+		</button>
+		<div class="headerTitle">{isPopoverOpened ? 'Фильтры' : 'Кейсы'}</div>
+	</div>
+	{#if $cases.isSuccess && !isPopoverOpened}
 		{#each $cases.data as { id, name, status, assignedAdmindId } (id)}
-			<div class="case">
+			<a class="case" href={`/cases/${id}`}>
 				<div class="caseInfo">
 					<div class="caseName">{name}</div>
 					{status}
@@ -34,12 +68,55 @@
 						{assignedAdmindId}
 					</div>
 				{/if}
-			</div>
+			</a>
 		{/each}
+	{/if}
+
+	{#if isPopoverOpened}
+		<div class="popover">
+			{#each filterRows as { title, filterId, buttons }}
+				<div class="popoverRow">
+					<div class="popoverTitle">{title}</div>
+					<div class="popoverButtons">
+						{#each buttons as { name, value }}
+							<div
+								class="popoverButton"
+								class:active={selectedFilters?.[filterId] === value}
+								on:click={() => toggleFilter(filterId, value)}
+							>
+								{name}
+							</div>
+						{/each}
+					</div>
+				</div>
+			{/each}
+			<div class="filterPopup">
+				<button type="button" on:click={() => (isPopoverOpened = false)}>Применить</button>
+			</div>
+		</div>
 	{/if}
 </div>
 
 <style lang="scss">
+	.header {
+		display: flex;
+		align-items: center;
+	}
+
+	.headerButton {
+		width: 32px;
+		height: 32px;
+		position: absolute;
+		border: none;
+		outline: none;
+	}
+
+	.headerTitle {
+		flex-grow: 1;
+		text-align: center;
+		font-size: 28px;
+		line-height: 42px;
+	}
 	.cases {
 		display: flex;
 		flex-direction: column;
@@ -76,5 +153,38 @@
 		font-size: 12px;
 		line-height: 18px;
 		color: #8d8d8d;
+	}
+
+	.popover {
+		position: absolute;
+		top: 80px;
+	}
+
+	.popoverRow {
+		margin-bottom: 8px;
+	}
+
+	.popoverTitle {
+		margin-bottom: 6px;
+		font-size: 17px;
+		line-height: 23.8px;
+	}
+
+	.popoverButtons {
+		display: flex;
+		gap: 12px;
+	}
+
+	.popoverButton {
+		padding: 6px 16px;
+		border-radius: 16px;
+		background: #f6f6f6;
+		font-size: 17px;
+		line-height: 23.8px;
+
+		&.active {
+			background: #8d8d8d;
+			color: #fff;
+		}
 	}
 </style>
