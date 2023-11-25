2c9356189ce2b276e4f6025296470bbaeb4cff22
hugeletters <ckuka71@gmail.com>
2023-11-25 23:57:43 +0100

    hotfix

--- src/routes/(auth)/(admin)/case/[id]/+page.svelte
+++ src/routes/(auth)/(admin)/case/[id]/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { statusList } from '$lib/options';
 	import Avia from '$lib/icons/avia.svelte';
 	import Bank from '$lib/icons/bank.svelte';
 	import Paper from '$lib/icons/paper.svelte';
+	import { statusList } from '$lib/options';
 
 	export let data;
 
@@ -20,58 +20,67 @@
 		}
 	);
 	const caseInfoMutation = data.trpc.case.updateCaseData.mutation();
-	const countries = new Map();
-	const categories = new Map();
-	$caseInfo.data.reports.forEach((report, index) => {
-		if (report.countries) {
-			countries.set(report.countries, index);
+	function iconByCategory(category: string) {
+		switch (category) {
+			case 'Банк':
+				return Bank;
+			case 'ВНЖ':
+				return Paper;
+			case 'Авиалиния':
+				return Avia;
 		}
-		if (report.categories) {
-			categories.set(report.categories, index);
-		}
-	});
+	}
+
+	function getUniqueValues<T>(arr: T[]) {
+		return [...new Set(arr.filter((x): x is NonNullable<T> => !!x))];
+	}
 </script>
 
-<div class="aboutCase">
-	<div class="statusWrapper">
-		<button
-			class={$caseInfo.data.status === 'active' ? 'status open' : 'status closed'}
-			on:click={() => {
-				$caseInfoMutation.mutate({
-					caseId: data.caseId,
-					newSummary: 'new summary',
-					newStatus: $caseInfo.data.status === statusList[0] ? statusList[1] : statusList[0]
-				});
+{#if $caseInfo.isSuccess}
+	{@const caseData = $caseInfo.data}
+	<div class="aboutCase">
+		<div class="statusWrapper">
+			<button
+				class={caseData.status === 'active' ? 'status open' : 'status closed'}
+				on:click={() => {
+					$caseInfoMutation.mutate({
+						caseId: data.caseId,
+						newSummary: 'new summary',
+						newStatus: caseData.status === statusList[0] ? statusList[1] : statusList[0]
+					});
 
-				data.trpc.case.caseInfo.utils.invalidate({ caseId: data.caseId });
-			}}
-		>
-			{$caseInfo.data.status.toUpperCase()}
-		</button>
-	</div>
-	<div>
-		<h4>Содержание</h4>
-		<p class="summary">
-			{$caseInfo.data?.summary || 'Пусто'}
-		</p>
-	</div>
-	<div>
-		<h4>Категории</h4>
-		<div class="countries">
-			{#each [...categories] as [key, val]}
-				<div class="country">{key},</div>
-			{/each}
+					data.trpc.case.caseInfo.utils.invalidate({ caseId: data.caseId });
+				}}
+			>
+				{caseData.status.toUpperCase()}
+			</button>
+		</div>
+		<div>
+			<h4>Содержание</h4>
+			<p class="summary">
+				{caseData?.summary || 'Пусто'}
+			</p>
+		</div>
+		<div>
+			<h4>Категории</h4>
+			<div class="countries">
+				{#each getUniqueValues(caseData.reports.map((x) => x.category)) as category}
+					<div class="country">
+						<svelte:component this={iconByCategory(category)} />
+					</div>
+				{/each}
+			</div>
+		</div>
+		<div>
+			<h4>Страны</h4>
+			<div class="countries">
+				{#each getUniqueValues(caseData.reports.map((x) => x.country)) as country}
+					<div class="country">{country}</div>
+				{/each}
+			</div>
 		</div>
 	</div>
-	<div>
-		<h4>Страны</h4>
-		<div class="countries">
-			{#each [...countries] as [key, val]}
-				<div class="country">{key},</div>
-			{/each}
-		</div>
-	</div>
-</div>
+{/if}
 
 <style lang="scss">
 	.aboutCase {
@@ -122,8 +131,5 @@
 		align-items: center;
 		justify-content: center;
 		margin-right: 16px;
-		span {
-			margin-left: 10px;
-		}
 	}
 </style>
