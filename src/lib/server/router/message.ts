336c28860360e97079814391f7979ec35ea74803
hugeletters <ckuka71@gmail.com>
2023-11-25 23:02:44 +0100

    hotfix

--- src/lib/server/router/message.ts
+++ src/lib/server/router/message.ts
@@ -1,9 +1,8 @@
-import { TRPCError } from '@trpc/server';
 import { asc, eq } from 'drizzle-orm';
 import { z } from 'zod';
 import { db } from '../database';
-import { Message, Report } from '../database/schema/report';
-import { adminProcedure, reportProcedure, router } from '../trpc';
+import { Message } from '../database/schema/report';
+import { reportProcedure, router } from '../trpc';
 import { throwInternalError } from './utils';
 
 export const messageRouter = router({
@@ -27,18 +26,5 @@ export const messageRouter = router({
 			.orderBy(asc(Message.createdAt))
 			.all()
 			.catch(throwInternalError);
-	}),
-	reassignToCase: adminProcedure
-		.input(z.object({ caseId: z.string(), reportId: z.string() }))
-		.mutation(({ input }) =>
-			db
-				.update(Report)
-				.set({ caseId: input.caseId })
-				.where(eq(Report.id, input.reportId))
-				.then((query) => {
-					if (!query.rowsAffected) {
-						throw new TRPCError({ code: 'BAD_REQUEST', message: 'Such report does not exist' });
-					}
-				}, throwInternalError)
-		)
+	})
 });
