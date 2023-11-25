e8ef6fbc00015a5d4ad01a8b033e9e71c758a432
Babulekh <vanca624@gmail.com>
2023-11-25 14:32:18 +0100

    Добавил компонент для ввода текста

---
+++ src/lib/components/textarea.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let text = '';
+</script>
+
+<textarea placeholder="Опишите суть проблемы" bind:value={text} />
+
+<style lang="scss">
+	textarea {
+		width: 100%;
+		height: 350px;
+
+		&::placeholder {
+			color: gray;
+		}
+	}
+</style>
--- src/routes/(auth)/+page.svelte
+++ src/routes/(auth)/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+	import Textarea from '$lib/components/textarea.svelte';
+
 	export let data;
 
 	const helloQuery = data.trpc.greet.hello.query(undefined, { staleTime: 5000 });
 	const usersQuery = data.trpc.greet.usernames.query();
+	let text: string;
 </script>
 
 <div>{$helloQuery.data} {data.session.user.name}</div>
 {#if $usersQuery.isSuccess}
-	{#each $usersQuery.data as usersName}
-		<div>{usersName.name}</div>
+	{#each $usersQuery.data as { name }}
+		<div>{name}</div>
 	{/each}
 {/if}
+
+<Textarea bind:text />
+
+{text}
