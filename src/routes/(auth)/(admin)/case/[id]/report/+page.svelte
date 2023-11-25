e262607fa2812ebf6800ad92b07912000f34d9c3
hugeletters <ckuka71@gmail.com>
2023-11-25 22:30:57 +0100

    thread and report pages

--- src/routes/(auth)/(admin)/case/[id]/report/+page.svelte
+++ src/routes/(auth)/(admin)/case/[id]/report/+page.svelte
@@ -39,9 +39,10 @@
 							</span>
 						</div>
 					</div>
-					<a class="caseLink link" href="/">Посмотреть кейс</a>
+					<!-- todo - fix this link - should lead to report info -->
+					<a class="caseLink link" href="/case/{data.caseId}/report/{chat.id}">Посмотреть кейс</a>
 				</div>
-				<a href="/" class="chatLink link">
+				<a href="/case/{data.caseId}/report/{chat.id}" class="chatLink link">
 					<svg
 						width="36"
 						height="36"
